734031bd3fa74021271c149ff4f050b206e62615
EL JADI Soufiane <eljadi.souf@gmail.com>
2024-12-14 19:45:51 +0100

    l

---
+++ ex01/Zombie.cpp
@@ -0,0 +1,24 @@
+ 
+#include "Zombie.hpp" 
+ 
+// Zombie:: Zombie()
+// {
+//     this->_name = "default";
+//     std::cout << this->_name<< " object created"<< std::endl;
+// }
+
+Zombie:: Zombie(std::string _name)
+{
+    this->_name = _name;
+    std::cout << this->_name<< " object created"<< std::endl;
+}
+
+Zombie::~ Zombie()
+{
+    std::cout << this->_name << " object destroyed"<< std::endl;
+}
+
+void	Zombie::announce()
+{
+	std::cout << this->_name << " BraiiiiiiinnnzzzZ..." << std::endl;
+}
