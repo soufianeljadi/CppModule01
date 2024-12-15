7b9f27d1bf28a51212a5b9ad7f4b4eac02a87759
EL JADI Soufiane <eljadi.souf@gmail.com>
2024-12-15 17:01:02 +0100

    finish ex01

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,9 +4,11 @@
 Zombie	*newZombie(std::string name);
 void	randomChump(std::string name);
 
+void f(){system("leaks zombie");}
 
 int main()
 {
+    atexit(f);
     Zombie z("ben 10");
     randomChump("foo");
 
