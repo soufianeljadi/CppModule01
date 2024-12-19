c648011970c53e0c558c8602d108a4300783a2b6
EL JADI Soufiane <eljadi.souf@gmail.com>
2024-12-19 20:16:50 +0100

    edits

---
+++ ex05/Harl.cpp
@@ -0,0 +1,53 @@
+
+#include "Harl.hpp"
+
+Harl:: Harl(/* args */)
+{
+}
+
+Harl::~ Harl()
+{
+}
+void Harl::debug( void )
+{
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger."<< std::endl;
+    std::cout << "I really do!" << std::endl<< std::endl;
+}
+void Harl::info( void )
+{
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money."<< std::endl;
+    std::cout << "You didn\'t put enough bacon in my burger!" << std::endl;
+    std::cout << "If you did, I wouldn\'t be asking for more!" << std::endl<< std::endl;
+}
+void Harl::warning( void )
+{
+
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free."<< std::endl;
+    std::cout << "I\'ve been coming for years whereas you started working here since last month."<< std::endl << std::endl;
+}
+void Harl::error( void )
+{
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout << "This is unacceptable!"<< std::endl;
+    std::cout << "I want to speak to the manager now." << std::endl<< std::endl;
+
+}
+void Harl::complain( std::string level )
+{
+    void (Harl::*ptr_complain[])(void) = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
+    std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+    
+    for (size_t i = 0; i < 4; i++) 
+    {
+        if (levels[i] == level) {
+            (this->*ptr_complain[i])();
+            return ; 
+        }
+    }
+    std::cerr << "[ UNRECOGNIZED ]"<<std::endl;
+    std::cerr  << level << " is an unknown level!" << std::endl<< std::endl;
+}
+
---
+++ ex05/main.cpp
@@ -0,0 +1,14 @@
+
+#include "Harl.hpp"
+
+int main()
+{
+    Harl soufiane;
+
+    soufiane.complain("DEBUG");
+    soufiane.complain("INFO");
+    soufiane.complain("WARNING");
+    soufiane.complain("ERROR");
+    soufiane.complain("ALERT");
+
+}
