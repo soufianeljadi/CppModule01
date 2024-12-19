eb3cea113faf730e366a43d67878a11b379d872d
EL JADI Soufiane <eljadi.souf@gmail.com>
2024-12-19 19:17:43 +0100

    finish

---
+++ ex06/Harl.cpp
@@ -0,0 +1,68 @@
+
+#include "Harl.hpp"
+
+Harl:: Harl(/* args */)
+{
+}
+
+Harl::~ Harl()
+{
+}
+void Harl::debug( void )
+{
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger."<< std::endl;
+    std::cout << "I really do!" << std::endl << std::endl;
+}
+void Harl::info( void )
+{
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money."<< std::endl;
+    std::cout << "You didn\'t put enough bacon in my burger!" << std::endl;
+    std::cout << "If you did, I wouldn\'t be asking for more!" << std::endl << std::endl;
+}
+void Harl::warning( void )
+{
+
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free."<< std::endl;
+    std::cout << "I\'ve been coming for years whereas you started working here since last month." << std::endl << std::endl;
+}
+void Harl::error( void )
+{
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout << "This is unacceptable!"<< std::endl;
+    std::cout << "I want to speak to the manager now." << std::endl << std::endl;
+
+}
+void Harl::complain( std::string level )
+{
+    const std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+    int levelIndex = -1;
+
+    for (int i = 0; i < 4; ++i) {
+        if (levels[i] == level) {
+            levelIndex = i;
+            break;
+        }
+    }
+
+    switch (levelIndex) {
+        case 0: 
+            debug();
+            // Fall through to INFO, WARNING, ERROR
+        case 1: 
+            info();
+            // Fall through to WARNING, ERROR
+        case 2:
+            warning();
+            // Fall through to ERROR
+        case 3:
+            error();
+            break;
+        default:
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+            break;
+    }
+}
+
